7fe43d92b2ee92bee12a2ff5df8a9d8fb7195d45
Sergey Pepyakin <s.pepyakin@gmail.com>
2016-05-21 20:24:18 +0300

    Instruction execution loop

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::path::Path;
 use std::io::Read;
 use std::fs::File;
 use std::env;
+use std::fmt;
 
 fn read_bin<P: AsRef<Path>>(path: P) -> Box<[u8]> {
     let mut bin_file = File::open(path).unwrap();
@@ -13,17 +14,16 @@ fn read_bin<P: AsRef<Path>>(path: P) -> Box<[u8]> {
 fn main() {
     let bin_file_name = env::args().nth(1).unwrap();
     let bin_data = read_bin(bin_file_name); 
-    
-    println!("{:?}", bin_data);
-    
+       
     let mut chip8 = Chip8::new();
     chip8.execute(bin_data);
 }
 
 struct Chip8 {
     memory: [u8; 4096],
-    gpr: [u8; 16]
-    // TODO: PC register
+    gpr: [u8; 16],
+    
+    pc: u16
     // TODO: I register
 }
 
@@ -31,20 +31,51 @@ impl Chip8 {
     fn new() -> Chip8 {
         Chip8 {
             memory: [0; 4096], // TODO: beware this stuff is going to be allocated on the stack
-            gpr: [0; 16]
+            gpr: [0; 16],
+            pc: 0x200
         }
     }
     
-    fn execute(&mut self, bin_data: Box<[u8]>) {
-        let first_byte = (*bin_data)[0] as u16;
-        let second_byte = (*bin_data)[1] as u16;
+    fn execute(&mut self, bin_data: Box<[u8]>) { 
+        loop {
+            let actual_pc = (self.pc - 0x200) as usize;
+            
+            let first_byte = (*bin_data)[actual_pc] as u16;
+            let second_byte = (*bin_data)[actual_pc + 1] as u16;
+            let instruction = (first_byte << 8) | second_byte;
+            
+            self.execute_instruction(instruction);
+            
+            println!("{:#?}", self);
+        }
+    }
+    
+    fn execute_instruction(&mut self, instruction: u16) {
+        println!("0x{:0x}", instruction);
         
-        let instruction = (first_byte << 8) | second_byte; 
-        
-        panic!("instruction: {:#x}", instruction);
+        if (instruction & 0xF000) == 0x1000 {
+            let destinationPC = instruction & 0x0FFF;
+            self.pc = destinationPC;
+            return
+        } else {
+            panic!("unrecognized instruction: {:#x}", instruction);
+        }
     }
     
     fn read_gpr(&self, index: u8) -> u8 {
         self.gpr[index as usize]
+    }
+}
+
+impl fmt::Debug for Chip8 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        try!(writeln!(f, "Chip8 {{"));
+        for i in 0..16 {
+            try!(writeln!(f, "  gpr{}: {:0x}", i, self.gpr[i]));
+        }
+        try!(writeln!(f, "  pc: {:0x}", self.pc));
+        try!(writeln!(f, "}}"));
+        
+        Ok(())
     }
 }

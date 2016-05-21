a2c01020eb726c7ee5a8f7543694b0c866a87170
Sergey Pepyakin <s.pepyakin@gmail.com>
2016-05-21 19:18:58 +0300

    Read binary data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::path::Path;
+use std::io::Read;
+use std::fs::File;
+use std::env;
+
+fn read_bin<P: AsRef<Path>>(path: P) -> Box<[u8]> {
+    let mut bin_file = File::open(path).unwrap();
+    let mut bin_buffer = Vec::new();
+    bin_file.read_to_end(&mut bin_buffer);
+    bin_buffer.into_boxed_slice()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bin_file_name = env::args().nth(0).unwrap();
+    let bin_data = read_bin(bin_file_name); 
+    
+    
 }

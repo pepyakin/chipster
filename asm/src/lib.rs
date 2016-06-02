523160326f68887a7cd9fc1549e522a8eafdff37
Sergey Pepyakin <s.pepyakin@gmail.com>
2016-06-02 22:10:37 +0300

    Move translate to own module

--- asm/src/lib.rs
+++ asm/src/lib.rs
@@ -4,103 +4,11 @@ extern crate combine;
 extern crate vm;
 
 mod parse;
-
-use vm::instruction::Addr;
-use vm::instruction::Imm;
-
-use parse::Operand;
-use parse::Statement;
-use parse::LiteralValue;
-
-impl LiteralValue {
-    fn as_imm(&self) -> Imm {
-        Imm(self.as_u8())
-    }
-
-    fn as_addr(&self) -> Addr {
-        Addr(self.as_u16())
-    }
-}
+mod translate;
 
 pub fn compile(source: &str) -> Box<[u8]> {
     let statements = parse::parse_source(source);
-
-    let mut instructions: Vec<vm::instruction::Instruction> = vec![];
-    for statement in statements.into_iter() {
-        match statement {
-            Statement::Instruction(mnemonic, operands) => {
-                println!("mnemonic={:?} operands={:?}", mnemonic, operands);
-                let instruction = match mnemonic.as_ref() {
-                    "CLS" => vm::instruction::Instruction::ClearScreen,
-                    "CALL" => {
-                        match &operands[..] {
-                            [Operand::Literal(lit)] => {
-                                vm::instruction::Instruction::Call(lit.as_addr())
-                            }
-                            _ => panic!("unsupported operand for CALL"),
-                        }
-                    }
-                    "SE" => {
-                        match &operands[..] {
-                            [Operand::Register(vx), Operand::Literal(kk)] => {
-                                vm::instruction::Instruction::SkipEqImm {
-                                    vx: vx,
-                                    imm: kk.as_imm(),
-                                    inv: false,
-                                }
-                            }
-                            [Operand::Register(vx), Operand::Register(vy)] => {
-                                vm::instruction::Instruction::SkipEqReg {
-                                    vx: vx,
-                                    vy: vy,
-                                    inv: false,
-                                }
-                            }
-                            _ => panic!("unsupported operands {:?}", operands),
-                        }
-                    }
-                    "SNE" => {
-                        match &operands[..] {
-                            [Operand::Register(vx), Operand::Literal(kk)] => {
-                                vm::instruction::Instruction::SkipEqImm {
-                                    vx: vx,
-                                    imm: kk.as_imm(),
-                                    inv: true,
-                                }
-                            }
-                            [Operand::Register(vx), Operand::Register(vy)] => {
-                                vm::instruction::Instruction::SkipEqReg {
-                                    vx: vx,
-                                    vy: vy,
-                                    inv: true,
-                                }
-                            }
-                            _ => panic!("unsupported operands {:?}", operands),
-                        }
-                    }
-                    _ => unimplemented!(),
-                };
-                instructions.push(instruction);
-            }
-            _ => {}
-        }
-    }
-
-    instructions.into_iter()
-        .flat_map::<Vec<u8>, _>(|instruction| {
-            fn unpack_word(word: u16) -> Vec<u8> {
-                // TODO: byteorder
-                let first_byte = ((word >> 8) & 0xFF) as u8;
-                let second_byte = (word & 0xFF) as u8;
-
-                vec![first_byte, second_byte]
-            }
-
-            let instruction_word = instruction.encode();
-            unpack_word(instruction_word.0)
-        })
-        .collect::<Vec<u8>>()
-        .into_boxed_slice()
+    translate::translate(statements)
 }
 
 #[test]
---
+++ asm/src/translate.rs
@@ -0,0 +1,102 @@
+
+use super::parse::*;
+
+use vm::instruction::Addr;
+use vm::instruction::Imm;
+use vm::instruction::Instruction;
+use super::vm;
+
+impl LiteralValue {
+    fn as_imm(&self) -> Imm {
+        Imm(self.as_u8())
+    }
+
+    fn as_addr(&self) -> Addr {
+        Addr(self.as_u16())
+    }
+}
+
+pub fn translate(statements: Vec<Statement>) -> Box<[u8]> {
+    let mut instructions: Vec<vm::instruction::Instruction> = vec![];
+    for statement in statements.into_iter() {
+        match statement {
+            Statement::Instruction(mnemonic, operands) => {
+                println!("mnemonic={:?} operands={:?}", mnemonic, operands);
+                let instruction = match_instruction(&mnemonic, operands);
+                instructions.push(instruction);
+            }
+            _ => {}
+        }
+    }
+
+    encode_instructions(instructions)
+}
+
+fn match_instruction(mnemonic: &str, operands: Vec<Operand>) -> vm::instruction::Instruction {
+    match mnemonic {
+        "CLS" => vm::instruction::Instruction::ClearScreen,
+        "CALL" => {
+            match &operands[..] {
+                [Operand::Literal(lit)] => vm::instruction::Instruction::Call(lit.as_addr()),
+                _ => panic!("unsupported operand for CALL"),
+            }
+        }
+        "SE" => {
+            match &operands[..] {
+                [Operand::Register(vx), Operand::Literal(kk)] => {
+                    vm::instruction::Instruction::SkipEqImm {
+                        vx: vx,
+                        imm: kk.as_imm(),
+                        inv: false,
+                    }
+                }
+                [Operand::Register(vx), Operand::Register(vy)] => {
+                    vm::instruction::Instruction::SkipEqReg {
+                        vx: vx,
+                        vy: vy,
+                        inv: false,
+                    }
+                }
+                _ => panic!("unsupported operands {:?}", operands),
+            }
+        }
+        "SNE" => {
+            match &operands[..] {
+                [Operand::Register(vx), Operand::Literal(kk)] => {
+                    vm::instruction::Instruction::SkipEqImm {
+                        vx: vx,
+                        imm: kk.as_imm(),
+                        inv: true,
+                    }
+                }
+                [Operand::Register(vx), Operand::Register(vy)] => {
+                    vm::instruction::Instruction::SkipEqReg {
+                        vx: vx,
+                        vy: vy,
+                        inv: true,
+                    }
+                }
+                _ => panic!("unsupported operands {:?}", operands),
+            }
+        }
+        _ => unimplemented!(),
+    }
+}
+
+fn encode_instructions(instructions: Vec<Instruction>) -> Box<[u8]> {
+    instructions.into_iter()
+        .flat_map::<Vec<u8>, _>(|instruction| {
+            fn unpack_word(word: u16) -> Vec<u8> {
+                // TODO: byteorder
+                let first_byte = ((word >> 8) & 0xFF) as u8;
+                let second_byte = (word & 0xFF) as u8;
+
+                vec![first_byte, second_byte]
+            }
+
+            let instruction_word = instruction.encode();
+            unpack_word(instruction_word.0)
+        })
+        .collect::<Vec<u8>>()
+        .into_boxed_slice()
+}

67c143f7d4858ce69392825b0d949a4cc65bf241
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-08-03 23:34:46 +0300

    extract loopers

--- chipster/src/main.rs
+++ chipster/src/main.rs
@@ -9,9 +9,6 @@ extern crate sdl2;
 #[macro_use]
 extern crate cfg_if;
 
-#[cfg(target_os = "emscripten")]
-pub mod emscripten;
-
 mod beep;
 mod render;
 
@@ -114,7 +111,7 @@ quick_main!(|| -> Result<()> {
     #[cfg(target_os = "emscripten")]
     let args = CommandArgs {
         rom_file_name: "file.rom".to_string(),
-        cycles_per_second: 500,
+        cycles_per_second: 15000,
         pixel_decay_time: 0.3,
     };
     
@@ -162,10 +159,14 @@ impl<'a, 'b: 'a, 'c> App<'a, 'b> {
 
         let mut last_ticks = timer.ticks();
 
-        let mut main_loop = || {
+        let mut main_loop = |done: &mut bool| {
             for event in events.poll_iter() {
                 match event {
-                    // Event::Quit { .. } => break 'main,
+                    Event::Quit { .. } => {
+                        *done = true;
+                        return;
+                    },
+
                     Event::KeyDown { keycode: Some(keycode), .. } => {
                         if let Some(pressed_key) = map_keycode(keycode) {
                             self.keyboard[pressed_key] = 1;
@@ -188,10 +189,12 @@ impl<'a, 'b: 'a, 'c> App<'a, 'b> {
         };
         
         #[cfg(target_os = "emscripten")]
-        emscripten::set_main_loop_callback(main_loop);
+        let looper = emscripten::EmscriptenLooper;
 
         #[cfg(not(target_os = "emscripten"))]
-        loop { main_loop(); }
+        let looper = DefaultLooper;
+
+        looper.start_loop(main_loop);
 
         Ok(())
     }
@@ -303,5 +306,80 @@ fn map_keycode(k: Keycode) -> Option<usize> {
         Keycode::C => Some(0xB),
         Keycode::V => Some(0xF),
         _ => None,
+    }
+}
+
+trait Looper {
+    fn start_loop<F>(self, f: F) where F: FnMut(&mut bool);
+}
+
+struct DefaultLooper;
+
+impl Looper for DefaultLooper {
+    fn start_loop<F>(self, mut f: F) where F: FnMut(&mut bool) {
+        use std::{thread, time};
+
+        let delay = time::Duration::from_millis(16);
+        loop {
+            let now = time::Instant::now();
+
+            let mut done = false;
+            f(&mut done);
+            if done {
+                break;
+            }
+
+            thread::sleep(delay);
+        }
+    }
+}
+
+#[cfg(target_os = "emscripten")]
+mod emscripten {
+    use std::cell::RefCell;
+    use std::ptr::null_mut;
+    use std::os::raw::{c_int, c_void, c_float};
+
+    #[allow(non_camel_case_types)]
+    type em_callback_func = unsafe extern fn();
+
+    #[link_args="-s USE_SDL=2"]
+    extern {
+        pub fn emscripten_set_main_loop(func: em_callback_func, fps: c_int, simulate_infinite_loop: c_int);
+        pub fn emscripten_cancel_main_loop();
+        pub fn emscripten_get_now() -> c_float;
+    }
+
+    thread_local!(static MAIN_LOOP_CALLBACK: RefCell<*mut c_void> = RefCell::new(null_mut()));
+
+    pub fn set_main_loop_callback<F>(callback: F) where F: FnMut() {
+        MAIN_LOOP_CALLBACK.with(|log| {
+            *log.borrow_mut() = &callback as *const _ as *mut c_void;
+        });
+
+        unsafe { emscripten_set_main_loop(wrapper::<F>, 0, 1); }
+
+        unsafe extern "C" fn wrapper<F>() where F: FnMut() {
+            MAIN_LOOP_CALLBACK.with(|z| {
+                let closure = *z.borrow_mut() as *mut F;
+                (*closure)();
+            });
+        }
+    }
+
+    pub struct EmscriptenLooper;
+
+    impl super::Looper for EmscriptenLooper {
+        fn start_loop<F>(self, mut f: F) where F: FnMut(&mut bool) {
+            set_main_loop_callback(|| {
+                let mut done = false;
+                f(&mut done);
+                if done {
+                    unsafe {
+                        emscripten_cancel_main_loop();
+                    }
+                }
+            })
+        }
     }
 }
